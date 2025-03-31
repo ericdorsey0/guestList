c889ab1bbd5663b6926422482119fa5ca7b98808
eric <dorseyjr000@gmail.com>
2025-03-30 22:54:58 -0500

    all

---
+++ src/useQuery.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2412-Eric";
+const API = BASE + COHORT;
+
+export default function useQuery(resource) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!resource) return;
+
+    const query = async () => {
+      setError(null);
+      try {
+        const response = await fetch(API + resource);
+        if (!response.ok) throw Error(`Could not query ${resource}.`);
+        const result = await response.json();
+        setData(result.data);
+      } catch (e) {
+        console.error(e);
+        setError(e.message);
+      }
+    };
+    query();
+  }, [resource]);
+
+  return { data, error };
+}
